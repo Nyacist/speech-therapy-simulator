5d69c64d7a27f025e90513cfd06a370a4628e8c1
Olga Kashirina <old.deus.73@gmail.com>
2024-05-16 16:13:27 +0300

    disable inputs

--- src/components/IntervalSettings.js
+++ src/components/IntervalSettings.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const IntervalSettings = ({min, max, step,
                             intervalValue, setIntervalValue,
                             buttonSettings, onClickButton}) => {
+  const [disableRange, setDisableRange] = useState(false)
   return (
     <div className="interval">
-      <input className="interval__input_range" type="range"
+      <input className="interval__input_range" type="range" disabled={disableRange}
              min={min} max={max} step={step}
              value={intervalValue}
              onChange={(e) => {setIntervalValue(e.target.value)}}
       />
-      <input className="interval__input_number" type="number"
+      <input className="interval__input_number" type="number" disabled={disableRange}
              min={min} max={max} step={step}
              value={intervalValue}
              onChange={(e) => {setIntervalValue(e.target.value)}}
       />
       <span>секунд</span>
       <button className={"button " + buttonSettings.style}
-              onClick={() => onClickButton()}
+              onClick={() => {
+                setDisableRange(!disableRange)
+                onClickButton()
+              }}
       >
         {buttonSettings.text}
       </button>
